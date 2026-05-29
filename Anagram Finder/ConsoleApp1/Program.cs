f3c0c46594b02faa735388ac08576056524606c5
MykeeBee <16993755+MykeeBee@users.noreply.github.com>
2026-05-29 23:38:38 +0100

    Add files via upload
    Added user input to allow picking of 4 different files for anagram length

--- Anagram Finder/ConsoleApp1/Program.cs
+++ Anagram Finder/ConsoleApp1/Program.cs
@@ -3,42 +3,64 @@
     class Program
     {
         static void Main()
-        {            
-            var wordListFile = File.ReadAllLines(@"..\..\..\4-letter-words-processed.txt");
-            var rawWordList = new List<string>(wordListFile);
-            var anagramGroups = new Dictionary<string, List<string>>();
+        {
+            Console.WriteLine("What length anagrams are you interested in?");
+            Console.WriteLine("Enter '4', '5', '6', '7' to pick an anagram length or anything else to exit");
 
-            foreach (var word in rawWordList)
+            string input = Console.ReadLine();
+            bool isValidNumber = int.TryParse(input, out int anagramLength);
+
+            if (isValidNumber && (anagramLength == 4 || anagramLength == 5 || anagramLength == 6 || anagramLength == 7))
             {
-                char[] charArray = word.ToCharArray();
-                Array.Sort(charArray);
-                var sortedKey = new string(word.OrderBy(c => c).ToArray());
+                string filePath = $@"..\..\..\{anagramLength}-letter-words.txt";
 
-                if (anagramGroups.ContainsKey(sortedKey))
+                if (File.Exists(filePath))
                 {
-                    anagramGroups[sortedKey].Add(word);
+                    var wordListFile = File.ReadAllLines(filePath);
+                    var rawWordList = new List<string>(wordListFile);
+                    var anagramGroups = new Dictionary<string, List<string>>();
+
+                    foreach (var word in rawWordList)
+                    {
+                        char[] charArray = word.ToCharArray();
+                        Array.Sort(charArray);
+                        var sortedKey = new string(word.OrderBy(c => c).ToArray());
+
+                        if (anagramGroups.ContainsKey(sortedKey))
+                        {
+                            anagramGroups[sortedKey].Add(word);
+                        }
+                        else
+                        {
+                            anagramGroups[sortedKey] = new List<string> { word };
+                        }
+                    }
+
+                    foreach (var anagramGroup in anagramGroups)
+                    {
+                        if (anagramGroup.Value.Count == 1)
+                        {
+                            anagramGroups.Remove(anagramGroup.Key);
+                        }
+                    }
+
+                    var sortedGroups = anagramGroups.OrderByDescending(x => x.Value.Count);
+
+                    using StreamWriter writer = new(@"..\..\..\anagram_results.txt");
+
+                    foreach (var anagramGroup in sortedGroups)
+                    {
+                        writer.WriteLine($"{anagramGroup.Key}: {string.Join(", ", anagramGroup.Value)}");
+                    }
                 }
                 else
                 {
-                    anagramGroups[sortedKey] = new List<string> { word };
+                    Console.WriteLine($"File not found: {filePath}");
                 }
             }
-
-            foreach (var anagramGroup in anagramGroups)
+            else
             {
-                if (anagramGroup.Value.Count == 1)
-                {
-                    anagramGroups.Remove(anagramGroup.Key);
-                }
-            }
-
-            var sortedGroups = anagramGroups.OrderByDescending(x => x.Value.Count);
-
-            using StreamWriter writer = new(@"..\..\..\anagram_results.txt");
-
-            foreach (var anagramGroup in sortedGroups)
-            {
-                writer.WriteLine($"{anagramGroup.Key}: {string.Join(", ", anagramGroup.Value)}");
+                Console.WriteLine("Exiting program...");
             }
         }
     }
